66259d3a3d29b4dad244555a20e54dee0085970d
rhpessoa <rhpessoa29@gmail.com>
2020-07-26 15:41:30 -0300

    First Commit

---
+++ lib/resultado.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class Resultado extends StatelessWidget {
+  final int pontuacao;
+  final void Function() quandoReiniciarQuestionario;
+
+  Resultado(this.pontuacao, this.quandoReiniciarQuestionario);
+
+  String get fraseResultado {
+    if (pontuacao == 3) {
+      return 'Você é o próprio Donodo!!';
+    } else if (pontuacao == 2) {
+      return 'Você Já ouviu sobre o Donodo!';
+    } else {
+      return 'Bora aprender mais né amigão';
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Center(
+          child: Text(
+            fraseResultado,
+            style: TextStyle(fontSize: 24),
+          ),
+        ),
+        FlatButton(
+          child: Text(
+            'Reiniciar?',
+            style: TextStyle(fontSize: 18),
+          ),
+          textColor: Colors.blue,
+          onPressed: quandoReiniciarQuestionario,
+        )
+      ],
+    );
+  }
+}
